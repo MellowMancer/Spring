a4181dc4aff2978e3e09c2568f4de11881c2ba4a
Rahul Deoghare <deogharerahul15@gmail.com>
2024-02-02 20:35:14 +0530

    made some changes in the dashboard

--- spring-frontend/lib/dashboard.dart
+++ spring-frontend/lib/dashboard.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: Dashboard(),
+  ));
+}
+
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key});
 
   static const String routeName = '/dashboard';
 
   @override
-  State<Dashboard> createState() => _DashboardState();
+  State<Dashboard> createState() => _DashboardState();        
 }
 
 class _DashboardState extends State<Dashboard> {
@@ -36,6 +42,7 @@ class _DashboardState extends State<Dashboard> {
         ),
         body: Container(
           padding: EdgeInsets.all(20.0),
+          color: _darkMode ? Colors.grey[900] ?? Colors.black : Colors.white,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.center,
@@ -48,7 +55,7 @@ class _DashboardState extends State<Dashboard> {
                 () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const SleepMonitoringScreen()),
+                    MaterialPageRoute(builder: (context) => SleepMonitoringScreen()),
                   );
                 },
               ),
@@ -61,7 +68,7 @@ class _DashboardState extends State<Dashboard> {
                 () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const MoodTrackingScreen()),
+                    MaterialPageRoute(builder: (context) => MoodTrackingScreen()),
                   );
                 },
               ),
@@ -74,7 +81,7 @@ class _DashboardState extends State<Dashboard> {
                 () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const GratitudeJournalingScreen()),
+                    MaterialPageRoute(builder: (context) => GratitudeJournalingScreen()),
                   );
                 },
               ),
@@ -87,7 +94,7 @@ class _DashboardState extends State<Dashboard> {
                 () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const DiaryScreen()),
+                    MaterialPageRoute(builder: (context) => DiaryScreen()),
                   );
                 },
               ),
@@ -110,15 +117,22 @@ class _DashboardState extends State<Dashboard> {
       child: Container(
         padding: EdgeInsets.all(20.0),
         decoration: BoxDecoration(
-          color: Theme.of(context).cardColor,
+          color: _darkMode ? Colors.grey[800] ?? Colors.black : Colors.grey[200] ?? Colors.white,
           borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: _darkMode ? Colors.grey[700] ?? Colors.black : Colors.grey[400] ?? Colors.black,
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ],
         ),
         child: Row(
           children: <Widget>[
             Icon(
               icon,
               size: 40,
-              color: Theme.of(context).primaryColor,
+              color: _darkMode ? Colors.white : Colors.black,
             ),
             SizedBox(width: 20),
             Column(
@@ -129,6 +143,7 @@ class _DashboardState extends State<Dashboard> {
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
+                    color: _darkMode ? Colors.white : Colors.black,
                   ),
                 ),
                 SizedBox(height: 5),
@@ -136,6 +151,7 @@ class _DashboardState extends State<Dashboard> {
                   description,
                   style: TextStyle(
                     fontSize: 16,
+                    color: _darkMode ? Colors.white70 : Colors.black87,
                   ),
                 ),
               ],
@@ -147,8 +163,36 @@ class _DashboardState extends State<Dashboard> {
   }
 }
 
-class SleepMonitoringScreen extends StatelessWidget {
+class SleepMonitoringScreen extends StatefulWidget {
   const SleepMonitoringScreen({Key? key});
+
+  @override
+  _SleepMonitoringScreenState createState() => _SleepMonitoringScreenState();
+}
+
+class _SleepMonitoringScreenState extends State<SleepMonitoringScreen> {
+  int _hoursSlept = 0;
+  String _qualityDescription = '';
+
+  void _calculateSleepQuality() {
+    if (_hoursSlept >= 8) {
+      setState(() {
+        _qualityDescription = 'Excellent';
+      });
+    } else if (_hoursSlept >= 6) {
+      setState(() {
+        _qualityDescription = 'Good';
+      });
+    } else if (_hoursSlept >= 4) {
+      setState(() {
+        _qualityDescription = 'Fair';
+      });
+    } else {
+      setState(() {
+        _qualityDescription = 'Poor';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -156,15 +200,102 @@ class SleepMonitoringScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Sleep Monitoring'),
       ),
-      body: const Center(
-        child: Text('Sleep Monitoring Screen'),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Hours Slept',
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _hoursSlept = int.tryParse(value) ?? 0;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                _calculateSleepQuality();
+              },
+              child: Text('Calculate Sleep Quality'),
+            ),
+            SizedBox(height: 20),
+            _qualityDescription.isNotEmpty
+                ? Column(
+                    children: [
+                      Text(
+                        'Sleep Quality: $_qualityDescription',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => SleepReportScreen(hoursSlept: _hoursSlept)),
+                          );
+                        },
+                        child: Text('View Detailed Report'),
+                      ),
+                    ],
+                  )
+                : SizedBox(),
+          ],
+        ),
       ),
     );
   }
 }
 
-class MoodTrackingScreen extends StatelessWidget {
+class SleepReportScreen extends StatelessWidget {
+  final int hoursSlept;
+
+  SleepReportScreen({required this.hoursSlept});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sleep Report'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Detailed Sleep Report',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'You slept for $hoursSlept hours last night.',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            // Add more details or analytics here
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MoodTrackingScreen extends StatefulWidget {
   const MoodTrackingScreen({Key? key});
+
+  @override
+  _MoodTrackingScreenState createState() => _MoodTrackingScreenState();
+}
+
+class _MoodTrackingScreenState extends State<MoodTrackingScreen> {
+  List<String> moods = ['Happy', 'Content', 'Neutral', 'Sad', 'Angry'];
+
+  String selectedMood = '';
 
   @override
   Widget build(BuildContext context) {
@@ -172,10 +303,81 @@ class MoodTrackingScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Mood Tracking'),
       ),
-      body: const Center(
-        child: Text('Mood Tracking Screen'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'How are you feeling today?',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: moods
+                .map(
+                  (mood) => GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedMood = mood;
+                      });
+                      // You can add logic here to save the mood entry
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: selectedMood == mood ? Colors.blue : Colors.grey[200],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        children: [
+                          Icon(
+                            _getMoodIcon(mood),
+                            size: 60,
+                            color: selectedMood == mood ? Colors.white : Colors.black,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            mood,
+                            style: TextStyle(
+                              color: selectedMood == mood ? Colors.white : Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+                .toList(),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // Add logic here to save mood entry with notes
+            },
+            child: Text('Save Mood'),
+          ),
+        ],
       ),
     );
+  }
+
+  IconData _getMoodIcon(String mood) {
+    switch (mood) {
+      case 'Happy':
+        return Icons.sentiment_very_satisfied;
+      case 'Content':
+        return Icons.sentiment_satisfied;
+      case 'Neutral':
+        return Icons.sentiment_neutral;
+      case 'Sad':
+        return Icons.sentiment_dissatisfied;
+      case 'Angry':
+        return Icons.sentiment_very_dissatisfied;
+      default:
+        return Icons.sentiment_neutral;
+    }
   }
 }
 
@@ -188,8 +390,34 @@ class GratitudeJournalingScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Gratitude Journaling'),
       ),
-      body: const Center(
-        child: Text('Gratitude Journaling Screen'),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Today\'s Gratitude',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextFormField(
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Oh I had such an amazing day today....',
+                    contentPadding: EdgeInsets.all(20),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -204,15 +432,39 @@ class DiaryScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Diary'),
       ),
-      body: const Center(
-        child: Text('Diary Screen'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Your Personal Diary',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/diary_bg.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.all(20),
+                child: TextFormField(
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Write your thoughts...',
+                    hintStyle: TextStyle(color: Colors.white.withOpacity(0.7)),
+                  ),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: Dashboard(),
-  ));
 }
--- spring-frontend/lib/main.dart
+++ spring-frontend/lib/main.dart
@@ -8,25 +8,27 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'home.dart';
+import 'dashboard.dart';
 import 'login.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-  );
-  if(kIsWeb){
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(apiKey: "AIzaSyCYe3ASqwa3ahPVYy8uXYfr9K0C6i2zRVw",
-      authDomain: "spring-a-ling.firebaseapp.com",
-      databaseURL: "https://spring-a-ling-default-rtdb.asia-southeast1.firebasedatabase.app",
-      projectId: "spring-a-ling",
-      storageBucket: "spring-a-ling.appspot.com",
-      messagingSenderId: "610922772594",
-      appId: "1:610922772594:web:e33e3664db737349f82aeb",
-      measurementId: "G-869YBQ6538"),
-    );
-  }
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //     options: DefaultFirebaseOptions.currentPlatform,
+  // );
+  // if(kIsWeb){
+  //   await Firebase.initializeApp(
+  //     options: const FirebaseOptions(apiKey: "AIzaSyCYe3ASqwa3ahPVYy8uXYfr9K0C6i2zRVw",
+  //     authDomain: "spring-a-ling.firebaseapp.com",
+  //     databaseURL: "https://spring-a-ling-default-rtdb.asia-southeast1.firebasedatabase.app",
+  //     projectId: "spring-a-ling",
+  //     storageBucket: "spring-a-ling.appspot.com",
+  //     messagingSenderId: "610922772594",
+  //     appId: "1:610922772594:web:e33e3664db737349f82aeb",
+  //     measurementId: "G-869YBQ6538"),
+  //   );
+  // }
   runApp(MyApp(settingsController: SettingsController(SettingsService())));
 
 } 
