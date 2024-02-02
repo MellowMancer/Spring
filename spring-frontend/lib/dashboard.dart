0f277ac00914d72a0353609e0f2bec8457454e1a
Rahul Deoghare <deogharerahul15@gmail.com>
2024-02-02 22:17:33 +0530

    made some more ui changes in the dashboard code

--- spring-frontend/lib/dashboard.dart
+++ spring-frontend/lib/dashboard.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -12,7 +14,7 @@ class Dashboard extends StatefulWidget {
   static const String routeName = '/dashboard';
 
   @override
-  State<Dashboard> createState() => _DashboardState();        
+  State<Dashboard> createState() => _DashboardState();
 }
 
 class _DashboardState extends State<Dashboard> {
@@ -163,6 +165,100 @@ class _DashboardState extends State<Dashboard> {
   }
 }
 
+// Mood Tracking Screen
+class MoodTrackingScreen extends StatelessWidget {
+  const MoodTrackingScreen({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mood Tracking'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'How are you feeling today?',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildMoodButton(context, "😄", "Happy", Colors.yellow),
+                _buildMoodButton(context, "😊", "Content", Colors.green),
+                _buildMoodButton(context, "😐", "Neutral", Colors.blueGrey),
+                _buildMoodButton(context, "😢", "Sad", Colors.blue),
+                _buildMoodButton(context, "😡", "Angry", Colors.red),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMoodButton(BuildContext context, String emoji, String label, Color color) {
+    return ElevatedButton(
+      onPressed: () {
+        _showSelectedMoodDialog(context, emoji, label);
+      },
+      style: ElevatedButton.styleFrom(
+        primary: color,
+        shape: CircleBorder(),
+        padding: EdgeInsets.all(24),
+      ),
+      child: Text(
+        emoji,
+        style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  void _showSelectedMoodDialog(BuildContext context, String emoji, String moodLabel) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          title: Text(
+            'You\'re feeling $moodLabel!',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                emoji,
+                style: TextStyle(fontSize: 64),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'That\'s awesome!',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'Close',
+                style: TextStyle(fontSize: 18, color: Colors.blue),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+// Sleep Monitoring Screen
 class SleepMonitoringScreen extends StatefulWidget {
   const SleepMonitoringScreen({Key? key});
 
@@ -172,27 +268,7 @@ class SleepMonitoringScreen extends StatefulWidget {
 
 class _SleepMonitoringScreenState extends State<SleepMonitoringScreen> {
   int _hoursSlept = 0;
-  String _qualityDescription = '';
-
-  void _calculateSleepQuality() {
-    if (_hoursSlept >= 8) {
-      setState(() {
-        _qualityDescription = 'Excellent';
-      });
-    } else if (_hoursSlept >= 6) {
-      setState(() {
-        _qualityDescription = 'Good';
-      });
-    } else if (_hoursSlept >= 4) {
-      setState(() {
-        _qualityDescription = 'Fair';
-      });
-    } else {
-      setState(() {
-        _qualityDescription = 'Poor';
-      });
-    }
-  }
+  double _sleepProgress = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -200,187 +276,70 @@ class _SleepMonitoringScreenState extends State<SleepMonitoringScreen> {
       appBar: AppBar(
         title: const Text('Sleep Monitoring'),
       ),
-      body: Padding(
+      body: Container(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: 'Hours Slept',
-              ),
+            Text(
+              'How many hours did you sleep?',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Slider(
+              value: _hoursSlept.toDouble(),
+              min: 0,
+              max: 12,
+              divisions: 12,
               onChanged: (value) {
                 setState(() {
-                  _hoursSlept = int.tryParse(value) ?? 0;
+                  _hoursSlept = value.toInt();
+                  _sleepProgress = value / 12;
                 });
               },
+              label: '$_hoursSlept',
+            ),
+            SizedBox(height: 20),
+            Text(
+              '$_hoursSlept hours',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(
+              value: _sleepProgress,
+              strokeWidth: 10,
+              backgroundColor: Colors.grey[300],
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                _calculateSleepQuality();
+                _calculateSleepQuality(_hoursSlept);
               },
               child: Text('Calculate Sleep Quality'),
             ),
-            SizedBox(height: 20),
-            _qualityDescription.isNotEmpty
-                ? Column(
-                    children: [
-                      Text(
-                        'Sleep Quality: $_qualityDescription',
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 10),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => SleepReportScreen(hoursSlept: _hoursSlept)),
-                          );
-                        },
-                        child: Text('View Detailed Report'),
-                      ),
-                    ],
-                  )
-                : SizedBox(),
           ],
         ),
       ),
     );
   }
-}
 
-class SleepReportScreen extends StatelessWidget {
-  final int hoursSlept;
-
-  SleepReportScreen({required this.hoursSlept});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sleep Report'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Detailed Sleep Report',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'You slept for $hoursSlept hours last night.',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 20),
-            // Add more details or analytics here
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class MoodTrackingScreen extends StatefulWidget {
-  const MoodTrackingScreen({Key? key});
-
-  @override
-  _MoodTrackingScreenState createState() => _MoodTrackingScreenState();
-}
-
-class _MoodTrackingScreenState extends State<MoodTrackingScreen> {
-  List<String> moods = ['Happy', 'Content', 'Neutral', 'Sad', 'Angry'];
-
-  String selectedMood = '';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Mood Tracking'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'How are you feeling today?',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: moods
-                .map(
-                  (mood) => GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedMood = mood;
-                      });
-                      // You can add logic here to save the mood entry
-                    },
-                    child: AnimatedContainer(
-                      duration: Duration(milliseconds: 300),
-                      curve: Curves.easeInOut,
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: selectedMood == mood ? Colors.blue : Colors.grey[200],
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Column(
-                        children: [
-                          Icon(
-                            _getMoodIcon(mood),
-                            size: 60,
-                            color: selectedMood == mood ? Colors.white : Colors.black,
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            mood,
-                            style: TextStyle(
-                              color: selectedMood == mood ? Colors.white : Colors.black,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                )
-                .toList(),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              // Add logic here to save mood entry with notes
-            },
-            child: Text('Save Mood'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  IconData _getMoodIcon(String mood) {
-    switch (mood) {
-      case 'Happy':
-        return Icons.sentiment_very_satisfied;
-      case 'Content':
-        return Icons.sentiment_satisfied;
-      case 'Neutral':
-        return Icons.sentiment_neutral;
-      case 'Sad':
-        return Icons.sentiment_dissatisfied;
-      case 'Angry':
-        return Icons.sentiment_very_dissatisfied;
-      default:
-        return Icons.sentiment_neutral;
+  void _calculateSleepQuality(int hoursSlept) {
+    String qualityDescription = '';
+    if (hoursSlept >= 8) {
+      qualityDescription = 'Excellent';
+    } else if (hoursSlept >= 6) {
+      qualityDescription = 'Good';
+    } else if (hoursSlept >= 4) {
+      qualityDescription = 'Fair';
+    } else {
+      qualityDescription = 'Poor';
     }
+    _showSnackBar(context, 'Sleep Quality: $qualityDescription');
   }
 }
 
+// Gratitude Journaling Screen
 class GratitudeJournalingScreen extends StatelessWidget {
   const GratitudeJournalingScreen({Key? key});
 
@@ -400,21 +359,20 @@ class GratitudeJournalingScreen extends StatelessWidget {
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: TextFormField(
-                  maxLines: null,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Oh I had such an amazing day today....',
-                    contentPadding: EdgeInsets.all(20),
-                  ),
-                ),
+            TextFormField(
+              maxLines: null,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'What are you grateful for today?',
+                contentPadding: EdgeInsets.all(20),
               ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                _showSnackBar(context, 'Gratitude recorded!');
+              },
+              child: Text('Save'),
             ),
           ],
         ),
@@ -423,8 +381,39 @@ class GratitudeJournalingScreen extends StatelessWidget {
   }
 }
 
-class DiaryScreen extends StatelessWidget {
+// Diary Screen
+class DiaryScreen extends StatefulWidget {
   const DiaryScreen({Key? key});
+
+  @override
+  _DiaryScreenState createState() => _DiaryScreenState();
+}
+
+class _DiaryScreenState extends State<DiaryScreen> {
+  late String _currentDate;
+  late String _currentTime;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeDateTime();
+  }
+
+  void _initializeDateTime() {
+    _updateDateTime();
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      if (!mounted) return;
+      setState(() {
+        _updateDateTime();
+      });
+    });
+  }
+
+  void _updateDateTime() {
+    final now = DateTime.now();
+    _currentDate = DateFormat.yMMMMd().format(now); // Example format: January 1, 2023
+    _currentTime = DateFormat.Hm().format(now); // Example format: 12:00 PM
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -432,39 +421,50 @@ class DiaryScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Diary'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Text(
               'Your Personal Diary',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
+            Text(
+              'Date: $_currentDate\nTime: $_currentTime',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
             Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/diary_bg.jpg'),
-                    fit: BoxFit.cover,
-                  ),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                padding: EdgeInsets.all(20),
-                child: TextFormField(
-                  maxLines: null,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Write your thoughts...',
-                    hintStyle: TextStyle(color: Colors.white.withOpacity(0.7)),
-                  ),
-                  style: TextStyle(color: Colors.white),
+              child: TextField(
+                maxLines: null,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Dear Diary...',
+                  contentPadding: EdgeInsets.all(20),
                 ),
               ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                _showSnackBar(context, 'Diary entry saved!');
+              },
+              child: Text('Save'),
             ),
           ],
         ),
       ),
     );
   }
+}
+
+void _showSnackBar(BuildContext context, String message) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(message),
+      duration: Duration(seconds: 1),
+    ),
+  );
 }
