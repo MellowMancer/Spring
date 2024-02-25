06cb57a35ac41eac9ab44133f8542fc1379e405d
MellowMancer <wyatharth@gmail.com>
2024-02-25 18:16:46 +0530

    Added app icon

--- spring-api/api/app.py
+++ spring-api/api/app.py
@@ -176,7 +176,7 @@ def sentiment():
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    host = '127.0.0.1'
+    host = '0.0.0.0'
     port = 5000
     print(f"Starting Flask app. Host: {host}, Port: {port}")
     app.run(debug=True, host=host, port=port)
